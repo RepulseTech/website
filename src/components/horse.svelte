c5edfad696066576bf3303fd8c0a226f5e903f79
John-Weak <tusharbali11@gmail.com>
2021-07-26 18:59:54 +0530

    init

---
+++ src/components/horse.svelte
@@ -0,0 +1,27 @@
+<div class="🐎">🐎</div>
+
+<style>
+	@font-face {
+		font-family: Muybridge;
+		src: url(https://www.lorp.org/fonts/MuybridgeGX.woff2);
+	}
+
+	@keyframes Gallop {
+		from {
+			font-variation-settings: 'TIME' 0;
+		}
+		to {
+			font-variation-settings: 'TIME' 15;
+		}
+	}
+
+	.🐎 {
+        display: inline-block;
+		font-family: Muybridge;
+		animation: 0.6s linear infinite Gallop;
+	}
+
+	.🐎:hover {
+		animation-duration: 6s;
+	}
+</style>
---
+++ src/routes/index.svelte
@@ -0,0 +1,13 @@
+<script>
+	import Horse from '../components/horse.svelte';
+</script>
+
+<div class="horse-outer">
+	<Horse />
+</div>
+
+<style>
+	.horse-outer {
+		font-size: 600px;
+	}
+</style>
